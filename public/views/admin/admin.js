84870b5fb85799006fcaee7356b7c9046cf26aef
locoBit <alan_herlo@hotmail.com>
2019-01-04 22:45:02 -0600

    Deleting unused service

--- public/views/admin/admin.js
+++ public/views/admin/admin.js
@@ -4,7 +4,7 @@ var admin = angular.module('admin', []);
 
 admin.component('admin', {
     templateUrl: 'views/admin/admin.html',
-    controller: function($scope, $http, $location, materiasService) {
+    controller: function($scope, $http, $location) {
 
       if ($scope.email === 'admin@admin.com') {
         $location.path('/adimn');
--- public/views/login/login.js
+++ public/views/login/login.js
@@ -4,7 +4,7 @@ var login = angular.module('login', []);
 
 login.component('login', {
     templateUrl: 'views/login/login.html',
-    controller: function($scope, $http, $location, materiasService) {
+    controller: function($scope, $http, $location) {
 
       $scope.submit = () => {
         if ($scope.email === 'admin@admin.com') {
